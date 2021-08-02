4b3763a989a3c811de019e21e8fe976af7112ff2
Fahim Abrar Saikat <fahimabrar.saikat@gmail.com>
2021-08-02 15:30:47 +0600

    Updated script

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,21 +6,22 @@ function run {
     $@&
   fi
 }
-
+run "xrandr --output DP2 --right-of eDP1 --auto"
 run "nm-applet"
-run "blueberry-tray"
+# run "blueberry-tray"
 run "numlockx on"
 run "volumeicon"
 run slstatus &
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+# run "conky -c $HOME/.config/conky/AUR-Skeleton.conkyrc"
 
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-ibus-daemon -drxR &
+# ibus-daemon -drxR &
 # dunst &
 # slstatus &
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
-nitrogen --set-scaled ~/.wallpaper.jpg &
+nitrogen --restore &
 imwheel -b 45 &
 picom -b --experimental-backends  --config ~/.config/picom/picom.conf &
 
