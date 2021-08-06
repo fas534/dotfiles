eaa93aa935268a5b85f2a9c97e076ffd8c2590bb
Fahim Abrar Saikat <fahimabrar.saikat@gmail.com>
2021-08-06 16:48:29 +0600

    Removed unnecessary apps

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,20 +6,18 @@ function run {
     $@&
   fi
 }
-run "xrandr --output DP2 --right-of eDP1 --auto"
-run "nm-applet"
-# run "blueberry-tray"
+
+# run "xrandr --output DP2 --right-of eDP1 --auto"
 run "numlockx on"
 run "volumeicon"
+run "nm-applet"
+# run "blueberry-tray"
 run slstatus &
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-# run "conky -c $HOME/.config/conky/AUR-Skeleton.conkyrc"
 
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # ibus-daemon -drxR &
 # dunst &
-# slstatus &
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
 nitrogen --restore &
 imwheel -b 45 &
